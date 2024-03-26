0025ca5025dd58b8e9283651a74321db10053f6c
Tijiena <a13816106692@163.com>
2024-03-26 18:59:00 -0400

    。

--- enlarge_image.js
+++ enlarge_image.js
@@ -5,6 +5,11 @@ document.querySelectorAll('.three_image img').forEach(image =>{
     document.querySelector('.popup-img img').src=image.getAttribute("src");}
 });
 
+document.querySelectorAll('.two_image img').forEach(image =>{
+    image.onclick=()=>{
+    document.querySelector('.popup-img').style.display='block';
+    document.querySelector('.popup-img img').src=image.getAttribute("src");}
+});
 document.querySelector('.popup-img span').onclick=()=>{
     document.querySelector('.popup-img').style.display="none";
 }
